9e2b08c4af7fee5bda049b634215701d31c5079f
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2019-07-11 12:47:53 +0530

    phasing from  remote computer

--- g.cpp
+++ g.cpp
@@ -1,3 +1,25 @@
 ajsoildf
 erisjf
 wpiejsf4sj
+sjdf
+__SIZEOF_DOUBLE__siodf
+__SIZEOF_DOUBLE__sidfc
+seidfc
+oiwdsfc
+isdfc
+psjodcx
+sidcz;'
+sdcps]dzc
+sdjfcxsdz'
+sdcj'sd
+sdjc
+s[
+    fasdi
+    as;df
+    cIDVCASJ
+    
+    RSDFOCJASDFA
+    S
+    SAIDZFJS
+    
+    SD'HXF'
